7a2a635a1b2138b0d65de3ff356eb40f7f7c4647
Sadjo Wora Diallo <geniewora@gmail.com>
2020-07-25 10:57:26 +0000

    On a realiser a ce stade 7 component de la page de demarrage

---
+++ src/index.js
@@ -0,0 +1,67 @@
+import React from "react"; 
+import ReactDOM from "react-dom";  
+import Menue from './components/barreMenue.jsx';
+import Footer from './components/footer.jsx';
+import Start from './components/showslide.js'; 
+import CardClient from './components/cardClient.jsx'; 
+import FormContact from './components/formulaireContact.jsx'; 
+import Template from './components/Template.jsx'; 
+import Codeur from './components/codeur.jsx';
+import Registre from './components/formRegistre.jsx';
+import Profile from './components/profile_menue.jsx';
+import Web from './components/web.jsx';
+import Login from './components/login.js';
+import "./styles/index.css";
+import "./styles/album.css";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from "react-router-dom";
+
+class App extends React.Component{
+   
+render(){
+   return (
+     <Router>
+      <div>
+          <Menue/>
+          <Switch>
+              <Route exact path="/">
+                <Start />
+                <CardClient />
+                <FormContact />
+                <Template />
+                <Codeur />
+                <Footer />
+              </Route>
+                <Route path="/formulaireContact">
+                  <FormContact/>
+                </Route>
+                  <Route path="/codeur">
+                    <Codeur/>
+                  </Route>
+                <Route path="/formRegistre">
+                  <Registre/>
+                </Route>
+               <Route path="/Login">
+                <Login />
+              </Route>
+              <Route path="/Profile">
+                <Profile />
+              </Route>
+               <Route path="/web">
+                   <Web/>
+                </Route>       
+          </Switch>
+      </div>
+     </Router>
+    );
+  }
+}
+
+
+ReactDOM.render(  
+    <App/>,
+    document.getElementById('root') 
+);
