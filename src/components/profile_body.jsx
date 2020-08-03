106a2b0c5bd6ccaa08572360ff6c44983ab628dc
Sadjo Wora Diallo <geniewora@gmail.com>
2020-08-03 12:39:38 +0000

    ajout de mise a jour sur la page dAceuil du profile

---
+++ src/components/profile_body.jsx
@@ -0,0 +1,34 @@
+import React from "react";  
+
+function ProfileBody(props){
+ 	return (
+ 		<div className="bodyProfile">
+ 			<div>
+ 			   <div> 
+	 			   <h1> Developper un site web.</h1> 
+	 			   <i className="badge badge-info">Web /</i>
+	 			   <i className="badge badge-info">Mobile/</i>
+	 			   <i className="badge badge-info">Outils/</i>
+ 			   </div>
+    		</div>
+    		<div>
+			   <p> 
+			   Le web devient par exellence le reseaux le plus utiliser au monde entre sites de rencontre
+			   de e-commerce et dIformation.				
+			   </p>
+			   <p >
+               Pour etre credible, visible et productif vous avez besoin dUne platforme dedié,
+               dUn blogue ou encor dUne appli de vente en ligne. Nous vous aidons par cela votre objectifs
+               aussi ambitier quIl soit.
+               </p>               
+               <p>
+               A lheur dInternet et de la digitalisation toutes entreprise ou similaire est dans le besion de 
+               ce munir dUn site web ou dUn application lui permettant dAugmenter sa productiviter. Avec nous vous 
+               avez juste quA demander la mission.
+               </p>    
+    		</div>
+ 		</div>
+ 		);
+ }
+
+export default ProfileBody; 
