ece8d8b18f92339533b13a631ffc9b04499186de
Sadjo Wora Diallo <geniewora@gmail.com>
2020-08-03 16:22:39 +0000

    Ajout des mise à jour sur la page pour web

--- src/components/web.jsx
+++ src/components/web.jsx
@@ -2,7 +2,16 @@ import React from "react";
 
 function  Web () {
   return (
-
+        <div className="row web">
+          <h1>Technologies de développement web</h1>
+            <p>
+               Les technologies du web permettent aujourd’hui de créer des applications informatiques qui n’auraient pas été possibles autrefois. 
+                   Des sites, et des applications web rapides, fluides et puissantes avec une ergonomie simple et efficace. 
+                   L'experience utilisateur dévient exeptionnel. Avec une telle technologie
+                   vous aurez plus de visibilité et plus  de productivité donc une augmentation sur votre chiffre d'affaire.
+                   Donner nous vos projets ayez confiance en nous, vous n'en sortrez que satisfait..                 
+            </p>
+        </div>
     );
 }
 export default Web;
