ef6cabb3bc9cf5d8f81300ecc22fe2417abd7534
Sadjo Wora Diallo <geniewora@gmail.com>
2020-08-05 13:08:12 +0000

    Mise a jour de la page aceuil du profile justqu'au footer

--- src/components/profile_body.jsx
+++ src/components/profile_body.jsx
@@ -2,30 +2,34 @@ import React from "react";
 
 function ProfileBody(props){
  	return (
- 		<div className="bodyProfile">
- 			<div>
- 			   <div> 
-	 			   <h1> Developper un site web.</h1> 
-	 			   <i className="badge badge-info">Web /</i>
-	 			   <i className="badge badge-info">Mobile/</i>
-	 			   <i className="badge badge-info">Outils/</i>
- 			   </div>
-    		</div>
-    		<div>
-			   <p> 
-			   Le web devient par exellence le reseaux le plus utiliser au monde entre sites de rencontre
-			   de e-commerce et dIformation.				
+ 		<div className="bodyProfile"> 		    		
+    	<div>
+			   <p className="my-card">
+          <h1> Developpeur, Codeur, Webmaster.</h1>     
+			   Le web devient par exellence la fenetre la plus utiliser au monde entre sites de rencontre
+			   de e-commerce et dIformation particuliere pour entreprise et application d'entreprise.			
 			   </p>
-			   <p >
-               Pour etre credible, visible et productif vous avez besoin dUne platforme dedié,
-               dUn blogue ou encor dUne appli de vente en ligne. Nous vous aidons par cela votre objectifs
-               aussi ambitier quIl soit.
-               </p>               
-               <p>
-               A lheur dInternet et de la digitalisation toutes entreprise ou similaire est dans le besion de 
-               ce munir dUn site web ou dUn application lui permettant dAugmenter sa productiviter. Avec nous vous 
-               avez juste quA demander la mission.
-               </p>    
+			   <p className="my-card">
+         <h3>Explosion dInternet</h3>
+           De nos jours il ya des millions de sites web et centaines de milliers d'application web ou mobile certaines connecter entre elles
+           et pour la plupart non. avec le temps plusieurs sources d'argent ce sont crée au tour du web entre des magasins de produits en ligne a des 
+           application generants des millions de dollard.
+           L'invention des smartphones par apple à donner une grande bouffé d'air au web, parceque plusieurs entreprises et marché entiere ce sont crée avec,
+           les entrepreneur on vue qu'avec un simple on pourvait ateindre un nombre enorme de peronne dans le monde.                      
+         <p>
+           Des Multinational comme #Google, #Facebook, #Amazone ou #twitter pour ne citer que cecis ils ce sont toutes Developper avec internet et ses technologie que sa soit mobile ou web.
+           Il gagne tous generalment des milliard de dollard par an, tous ça vous montre à quel point le web est devenue tres puissants.
+         </p>
+            <p> <img src="entreprise.png"  alt="entreprise"  /></p>
+         </p>
+         <p className="my-card">
+           <h3>Developpeur ou Webmaster</h3>
+           Au debut on à eu du male à definir le vrai difference des deux mai avec la naissance des technologies du web fontend les roles ces sont mieux dessiner
+          le Developpeur est le concepteur du sites il est celui qui à des conaissances plus pousser dans les languages du web, le webmaster est un couto suisse il 
+          est bon à tous il developpe des petits sites ger a production et le contenue quand site devient une application plus complexe il fait appel au specialiste 
+          donc deeloppeur.
+
+          </p>
     		</div>
  		</div>
  		);
